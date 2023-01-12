b428e390bfc796cf0d1c764f6ab43a20132e5bba
epiccreeper10 <87875284+epiccreeper10@users.noreply.github.com>
2023-01-12 09:42:38 -0700

    Spawner
    Spawn an agent every time you click

--- Assets/Scripts/Autonomous Agent/AutonomousAgent.cs
+++ Assets/Scripts/Autonomous Agent/AutonomousAgent.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class AutonomousAgent : Agent
 {
 
+    public float wanderDistance = 1;
+    public float wanderRadius = 3;
+    public float wanderDisplacement = 5;
+
+    public float wanderAngle { get; set; } = 0;
+
     void Update()
     {
         var gameObjects = perception.GetGameObjects();
@@ -12,13 +18,24 @@ public class AutonomousAgent : Agent
         {
             Debug.DrawLine(transform.position, gameObject.transform.position);
         }
+        if (gameObjects.Length == 0)
+        {
+            if (movement.acceleration.sqrMagnitude <= movement.maxForce * 0.1f)
+            {
+                movement.ApplyForce(Steering.Wander(this));
+            }
+        }
+
+
         if (gameObjects.Length > 0 && gameObjects[0] != null) 
         {
-            movement.ApplyForce(Steering.Seek(this, gameObjects[0]) * 1);
-            movement.ApplyForce(Steering.Flee(this, gameObjects[0]) * 0);
+            movement.ApplyForce(Steering.Seek(this, gameObjects[0]) * 0);
+            movement.ApplyForce(Steering.Flee(this, gameObjects[0]) * 1);
         }
 
         transform.position = Utilities.Wrap(transform.position, new Vector3(-10, -10, -10), new Vector3(10, 10, 10));
-        
+
+
+
     }
 }
